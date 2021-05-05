996ec8a088851731ff2213fb2800fc603c803934
Bogusław Piskulak <boguslaw.piskulak@gmail.com>
2021-05-05 20:46:57 +0200

    adding new tasks and showing if a task is done

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@
     const tasks = [
         {
             content: "Nagrać lekcje",
-            dane: false,
+            done: false,
         },
         {
             content: "Dodać lekcje",
-            dane: true,
+            done: true,
         },
     ]
 
@@ -16,7 +16,9 @@
 
         for (const task of tasks) {
             htmlString += `
-                <li>
+            <li
+                ${task.done ? " style=\"text-decoration: line-through\"" : ""}
+            >
                 ${task.content}
             </li>
             `;
@@ -24,9 +26,30 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
-    const init = () => {
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
         render();
     };
-  
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", onFormSubmit);
+    };
     init();
 }
