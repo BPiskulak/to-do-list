97947f39f45d88c3bb1430cf8399bc7b843e2337
Bogusław Piskulak <boguslaw.piskulak@gmail.com>
2021-05-11 13:56:38 +0200

    update css - js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@
         tasks.push({
             content: newTaskContent,
         });
-
         render();
     };
 
@@ -29,7 +28,6 @@
 
     const toggleTaskDone = (taskIndex) => {
         tasks[taskIndex].done = !tasks[taskIndex].done;
-
         render();
     }
 
@@ -39,9 +37,7 @@
         removeButtons.forEach((removeButtons, index) => {
             removeButtons.addEventListener("click", () => {
                 removeTask(index);
-
             });
-
         });
         const togglleDonebuttons = document.querySelectorAll(".js-done");
         togglleDonebuttons.forEach((togglleDonebuttons, index) => {
@@ -59,7 +55,7 @@
         for (const task of tasks) {
             htmlString += `
             <li class="taskList__content">
-                <button class="taskList__button taskList__button--done js-done">
+                <button class="taskList__button js-done">
                     ${task.done ? "&#x2714;" : " "}
                 </button>
                 <span class="task ${task.done ? "taskList__content--done\"" : "\""}>
@@ -69,17 +65,12 @@
             </li>
             `;
         }
-
-
-
         document.querySelector(".js-tasks").innerHTML = htmlString;
-
        bindEvents();
     };
 
     const onFormSubmit = (event) => {
         event.preventDefault();
-
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
         const newTask = document.querySelector(".js-newTask");
         if (newTaskContent === "") {
